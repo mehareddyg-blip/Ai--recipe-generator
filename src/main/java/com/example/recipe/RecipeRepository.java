072fdaec639a7d851bacdc18dce4110294a0aa93
mehareddyg-blip <mehareddyg@gmail.com>
2026-09-13 19:23:45 -0500

    Create RecipeRepository

---
+++ src/main/java/com/example/recipe/RecipeRepository.java
@@ -0,0 +1,9 @@
+package com.example.recipe;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+    List<Recipe> findAll();
+}
